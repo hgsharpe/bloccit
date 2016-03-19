e7e07bc8a9386991451904982636ae52b86008d7
hgsharpe <henrygsharpe@gmail.com>
2016-03-19 19:15:03 +0000

    31-intro-crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+  
+  resources :posts
+
   get 'welcome/index'
   
   get 'welcome/about'
+  get 'about' => 'welcome#about'
   
   get 'welcome/contact'
   
