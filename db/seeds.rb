5d6a578e3c99a3d67e7d6f82f730b0eb5ba0a481
hgsharpe <henrygsharpe@gmail.com>
2016-03-24 16:04:38 +0000

    33-more-crud-assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,34 @@
-require 'random_data'
+include RandomData
 
+# Create Posts
 50.times do
-    
   Post.create!(
-      title: RandomData.random_sentence,
-      body: RandomData.random_paragraph
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
     )    
 end
   
 posts = Post.all
-  
+
+# Create Comments  
 100.times do
   Comment.create!(
-        
     post: posts.sample,
     body: RandomData.random_paragraph
     )
 end
 
-puts "#{Post.count}"
-Post.find_or_create_by(title: "A unique title", body: "A unique body")
-puts "#{Post.count}"
+# Create Questions
+100.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+    )
+end
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) { Question.new(title: "New Question Body", body: "New Question Body", resolved: false) }
-  
-  it "should respond to title" do
+  context "attributes" do
+    let(:question) { Question.new(title: "New Question Title", body: "New Question Body",
+    resolved: false) }
+    
+    it "should respond to title" do
       expect(question).to respond_to(:title)
-  end
+    end
   
-  it "should respond to body" do
+    it "should respond to body" do
       expect(question).to respond_to(:body)
-  end
+    end
   
-  it "should respond to" do
+    it "should respond to resolved" do
       expect(question).to respond_to(:resolved)
+    end
   end
-  
 end
