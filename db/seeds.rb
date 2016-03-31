e15f4728930abd27125d260664a51b18921bc17e
Conrad Taylor <conradwt@gmail.com>
2016-03-31 16:41:30 -0700

    Code cleanup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,59 +3,63 @@ puts "Seeding database:\n"
 puts "Seeding Topics, Posts, Sponsored Posts, Comments, Advertisements, Questions"
 
 # Create Topics
+
 10.times do
   Topic.create!(
-    name:        RandomData.random_sentence,
-    description: RandomData.random_paragraph
+  name:        RandomData.random_sentence,
+  description: RandomData.random_paragraph
   )
 end
+
 topics = Topic.all
 
 # Create Posts
+
 50.times do
   Post.create!(
-     topic:  topics.sample,
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
+  topic:  topics.sample,
+  title:  RandomData.random_sentence,
+  body:   RandomData.random_paragraph
   )
 end
+
 # Create one more Topic and Post, unique
-unique_topic = Topic.find_or_create_by!(name: 'Celebrities', description: 'Actresses')
-Post.find_or_create_by!(topic: unique_topic, title: 'Scarlett', body: 'Johansson')
+unique_topic = Topic.find_or_create_by!(name: 'Celebrities', description: RandomData.random_paragraph)
+Post.find_or_create_by!(topic: unique_topic, title: 'Scarlett', body: RandomData.random_paragraph)
 posts = Post.all
 
 # Create Comments
 100.times do
   Comment.create!(
-    post:  posts.sample,
-    body:  RandomData.random_paragraph
+  post:  posts.sample,
+  body:  RandomData.random_paragraph
   )
 end
 
 # Create Sponsored Posts
 5.times do
   SponsoredPost.create!(
-      topic:  topics.sample,
-      title:  RandomData.random_sentence,
-      body:   RandomData.random_paragraph
+  topic:  topics.sample,
+  title:  RandomData.random_sentence,
+  body:   RandomData.random_paragraph
   )
 end
 
 # Create Advertisements
 10.times do
   Advertisement.create!(
-     title: RandomData.random_sentence,
-     copy:  RandomData.random_paragraph,
-     price: RandomData.random_ad_price
+  title: RandomData.random_sentence,
+  copy:  RandomData.random_paragraph,
+  price: RandomData.random_sponsored_post_price
   )
 end
 
 # Create Questions
 10.times do
   Question.create!(
-    title: RandomData.random_sentence,
-    body:  RandomData.random_paragraph,
-    resolved: rand(1..2) == 1
+  title: RandomData.random_sentence,
+  body:  RandomData.random_paragraph,
+  resolved: rand(1..2) == 1
   )
 end
 
