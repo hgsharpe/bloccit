ff5ca80e89ab417e7621aeb14dc472a9e7fdecb0
hgsharpe <henrygsharpe@gmail.com>
2016-04-01 02:25:26 +0000

    36-user-model-assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 # #2 we register an inline callback directly after the before_save     
    before_save { self.email = email.downcase }
+   before_save :format_name
 
  # #3 we use Ruby's validates function to ensure that name is present and has a maximum and minimum length.
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -8,11 +9,22 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
  # #5 we validate that email is present, unique, case insensitive, has a minimum length, has a maximum length
-   validates :email,
+  validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
 
  # #6 we use Ruby's has_secure_password
    has_secure_password
+   
+   def format_name
+      if name
+        name_array = []
+        name.split.each do |name_part|
+          name_array << name_part.capitalize
+        end
+        
+        self.name = name_array.join(" ")
+      end
+   end
 end
