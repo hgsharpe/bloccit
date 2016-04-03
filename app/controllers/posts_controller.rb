f61f50acc803ea2941fa22bd3e0a0e07ff898dc5
Conrad Taylor <conradwt@gmail.com>
2016-04-03 02:50:06 -0700

    Fix - post routes and actions.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def show
-    @posts = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:id])
   end
 
   def new
     @topic = Topic.find(params[:topic_id])
-    @post = Post.new
+    @post = @topic.posts.build
   end
 
   def create
@@ -25,11 +26,13 @@ class PostsController < ApplicationController
   end
   
   def edit
-    @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:id])
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
  
@@ -42,15 +45,16 @@ class PostsController < ApplicationController
     end
   end
   
- def destroy
-    @post = Post.find(params[:id])
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:id])
     
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
       redirect_to @post.topic
     else
-     flash.now[:alert] = "There was an error deleting the post."
-     render :show
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
     end
- end
+  end
 end
