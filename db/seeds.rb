cd7274a6a8936e9ca08d959e57bc5fb70e8a0806
hgsharpe <henrygsharpe@gmail.com>
2016-04-04 18:43:55 +0000

    40-authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,12 +69,19 @@ end
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'youremail.com', # replace this with your personal email
-  password: 'helloworld'
- )
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
  
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
+
 puts 'Seed finished'
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
