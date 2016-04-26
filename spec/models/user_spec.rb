9ed75957029d44cd376689edc122c4dd553ffba7
hgsharpe <henrygsharpe@gmail.com>
2016-04-26 13:42:51 +0000

    44-Favorites

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe User, type: :model do
   
    it { is_expected.to have_many(:posts) }
    it { is_expected.to have_many(:comments) }
-   it { is_expected.to have_many(:votes) }   
+   it { is_expected.to have_many(:votes) } 
+   it { is_expected.to have_many(:favorites) }
    it { is_expected.to validate_presence_of(:name) }
    it { is_expected.to validate_length_of(:name).is_at_least(1) }
  
@@ -66,7 +67,6 @@ RSpec.describe User, type: :model do
          expect(user.admin?).to be_truthy
        end
      end
-   end
  
      it "should respond to email" do
        expect(user).to respond_to(:email)
@@ -77,7 +77,7 @@ RSpec.describe User, type: :model do
         user.save
         expect(user.name).to eq "Any User"
      end
- end
+   end
      
     describe "invalid user" do
      let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
@@ -91,3 +91,20 @@ RSpec.describe User, type: :model do
        expect(user_with_invalid_email).to_not be_valid
      end
    end
+   
+   describe "#favorite_for(post)" do
+     before do
+       topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+       @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+     end
+ 
+     it "returns `nil` if the user has not favorited the post" do
+       expect(user.favorite_for(@post)).to be_nil
+     end
+ 
+     it "returns the appropriate favorite if it exists" do
+       favorite = user.favorites.where(post: @post).create
+       expect(user.favorite_for(@post)).to eq(favorite)
+     end
+   end
+ end
